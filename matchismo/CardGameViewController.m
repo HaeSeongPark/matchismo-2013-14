ab5bf78616d72f7a79af0566b29acdb5d2b090a7
rhino <cord7894@gmail.com>
2019-02-14 20:04:48 +0900

    Add: to clean unnecessary old code and make outlet collections and insert cardmatchinggame

--- matchismo/CardGameViewController.m
+++ matchismo/CardGameViewController.m
@@ -9,46 +9,27 @@
 #import "CardGameViewController.h"
 #import "Model/Deck.h"
 #import "Model/PlayingCardDeck.h"
+#import "Model/CardMatchingGame.h"
 
 @interface CardGameViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
-@property (nonatomic) int flipCount;
 @property (strong, nonatomic) Deck *deck;
-
+@property (nonatomic, strong) CardMatchingGame *game;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 @end
 
 @implementation CardGameViewController
 
-- (Deck *)deck {
-    if (!_deck) _deck = [self createDeck];
-    return _deck;
+- (CardMatchingGame *)game {
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:[self.cardButtons count] usingDeck:[self createDeck]];
+    return _game;
 }
 
 -(Deck *) createDeck {
     return [PlayingCardDeck new];
 }
 
-- (void)setFlipCount:(int)flipCount
-{
-    _flipCount = flipCount;
-    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-
-}
-
 - (IBAction)touchCardButton:(UIButton *)sender {
-    if ( [sender.currentTitle length]) {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardBack"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"" forState:UIControlStateNormal];
-        self.flipCount++;
-    } else {
-        Card *card = [self.deck drawRandomCard];
-        if ( card ) {
-            [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"] forState:UIControlStateNormal];
-            [sender setTitle:card.contents forState:UIControlStateNormal];
-            self.flipCount++;
-        }
-    }
+    
 }
 
 @end
