43887a191ec5a791980d64b934d5f453f527a707
rhino <cord7894@gmail.com>
2019-02-14 19:54:26 +0900

    Add: CardMatchingGame

---
+++ matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,25 @@
+//
+//  CardMatchingGame.h
+//  matchismo
+//
+//  Created by rhino Q on 14/02/2019.
+//  Copyright © 2019 rhino Q. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CardMatchingGame : NSObject
+
+// designated initializer
+-(instancetype)initWithCardCount:(NSUInteger)count
+                       usingDeck: (Deck*)deck;
+
+-(void)chooseCardAtIndex:(NSUInteger)index;
+-(Card *)cardAtIndex: (NSUInteger)index;
+
+@property (nonatomic, readonly) NSInteger score;
+@end
+
+NS_ASSUME_NONNULL_END
