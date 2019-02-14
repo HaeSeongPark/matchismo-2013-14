c0bf1c4f8483cd72b3aefe616b398afbf4bf0316
rhino <cord7894@gmail.com>
2019-02-14 20:33:42 +0900

    Add : change CardGame logic

--- matchismo/CardGameViewController.m
+++ matchismo/CardGameViewController.m
@@ -15,6 +15,7 @@
 @property (strong, nonatomic) Deck *deck;
 @property (nonatomic, strong) CardMatchingGame *game;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation CardGameViewController
@@ -29,7 +30,28 @@
 }
 
 - (IBAction)touchCardButton:(UIButton *)sender {
-    
+    int cardIndex = (int)[self.cardButtons indexOfObject:sender];
+    [self.game chooseCardAtIndex:cardIndex];
+    [self updateUI];
 }
 
+-(void)updateUI {
+    for (UIButton *cardButton in self.cardButtons) {
+        int cardIndex = (int)[self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardIndex];
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMached;
+    }
+    
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score : %d", (int)self.game.score];
+}
+
+-(NSString *)titleForCard: (Card*)card {
+    return card.isChosen ? card.contents : @"";
+}
+
+-(UIImage *)backgroundImageForCard:(Card *)card {
+    return [UIImage imageNamed: card.isChosen ? @"cardFront" : @"cardBack"];
+}
 @end
